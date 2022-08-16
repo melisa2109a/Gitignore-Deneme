44a68aba27632c46d2f1014fcda41efb6117a5e3
melisa_akçay <melisaakcay69@gmail.com>
2022-08-16 22:59:54 +0300

    mnbm

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -1,5 +1,7 @@
 public class Gitignore {
     public static void main(String[] args) {
         System.out.println("Gitignore Deneme 1");
+        System.out.println("Gitignore Deneme 2");
+
     }
 }
